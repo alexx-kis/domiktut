aafa2b9e7d3a32e10ead090fa65320c5592814eb
Alexander <fenaethylaamiinloever@gmail.com>
2023-08-18 19:22:07 +0300

    +animation

--- js/script.js
+++ js/script.js
@@ -327,3 +327,37 @@ window.onload = function () {
 		loading.classList.add('loaded');
 	}
 };
+
+let animItems = document.querySelectorAll('.anim-item');
+
+if (animItems.length > 0) {
+	window.addEventListener('scroll', animOnScroll);
+	function animOnScroll() {
+		for (let i = 0; i < animItems.length; i++) {
+			let animItem = animItems[i];
+			let animItemHeight = animItem.offsetHeight;
+			let animItemOffset = offset(animItem).top;
+			let animStart = 4
+			let animItemPoint = window.innerHeight - (animItemHeight / animStart);
+
+			if (animItemHeight > window.innerHeight) {
+				animItemPoint = window.innerHeight - (window.innerHeight / animStart);
+			}
+
+			if ((pageYOffset > animItemOffset - animItemPoint) && (pageYOffset < (animItemOffset + animItemHeight))) {
+				animItem.classList.add('anim-item--active');
+			} else {
+				if (!animItem.classList.contains('anim-no-hide')) {
+					animItem.classList.remove('anim-item--active');
+				}
+			}
+
+			function offset(elem) {
+				let rect = elem.getBoundingClientRect(),
+					scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+					scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+				return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+			}
+		}
+	}
+}
